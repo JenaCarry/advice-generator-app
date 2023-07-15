13d955cbc2a1e620cf6080ad27fee938ebbf401f
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-07-15 20:28:22 -0300

    .

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Divider } from "./Divider";
 
 interface AdviceProps {
   id: number;
@@ -10,7 +11,6 @@ interface AdviceProps {
 
 export function Advice() {
   const [adviceApi, setAdviceApi] = useState<AdviceProps | null>(null);
-  const [initialAdvice, setInitialAdvice] = useState<AdviceProps | null>(null);
   const [buttonAdvice, setButtonAdvice] = useState("");
 
   function fetchApi() {
@@ -36,11 +36,12 @@ export function Advice() {
   return (
     <section className="text-center relative px-7 pt-10 pb-16 min-[481px]:px-11">
       <h2 className="text-sm text-neon-green uppercase tracking-[0.25em] mb-8">
-        Advice # {adviceApi?.id ? adviceApi.id : initialAdvice?.id}
+        Advice # {adviceApi?.id}
       </h2>
       <p className="max-[481px]:text-[1.375rem] text-[1.75rem] mb-6">
-        {adviceApi?.advice ? adviceApi.advice : initialAdvice?.advice}
+        {adviceApi?.advice}
       </p>
+      <Divider />
       <div className="absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2">
         <button
           className={`${"bg-neon-green p-5 rounded-full hover:shadow-[0_0_30px_0_rgb(82,255,168)]"} ${buttonAdvice}`}
